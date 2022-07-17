cdf7ca08f356909e5713673d6b20dd652a6deaca
fadiabji <fadi.abji@htomail.com>
2022-07-17 14:46:48 +0200

    create a sperate class for settings and but the settings variable in it

--- alien_invation.py
+++ alien_invation.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvation:
     """Overall calss to mange game assets and behavior."""
@@ -7,13 +8,12 @@ class AlienInvation:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
-        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invation")
-
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -24,7 +24,7 @@ class AlienInvation:
                     sys.exit()
 
             #redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -33,3 +33,4 @@ if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvation()
     ai.run_game()
+
