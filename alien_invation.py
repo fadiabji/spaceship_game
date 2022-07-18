597f6e3864244902a55883879b213a4b52362692
fadiabji <fadi.abji@htomail.com>
2022-07-18 13:56:46 +0200

    After refactoring the methods

--- alien_invation.py
+++ alien_invation.py
@@ -10,13 +10,11 @@ class AlienInvation:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-
         pygame.display.set_caption("Alien Invation")
-
         self.ship = Ship(self)
+
 
     def run_game(self): 
         """Start the main loop for the game."""
@@ -32,29 +30,38 @@ class AlienInvation:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-            
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
-        """This method called helper method, it prfixs with _"""
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         pygame.display.flip()
 
-
-
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvation()
     ai.run_game()
+
+
+
+
+
+
 
