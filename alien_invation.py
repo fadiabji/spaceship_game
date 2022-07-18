10c20376c07d296dd30f581fa5e7edae27cdbc7f
fadiabji <fadi.abji@htomail.com>
2022-07-18 12:12:20 +0200

    adding a spaceship to the screen

--- alien_invation.py
+++ alien_invation.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvation:
     """Overall calss to mange game assets and behavior."""
@@ -15,6 +16,8 @@ class AlienInvation:
 
         pygame.display.set_caption("Alien Invation")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -25,6 +28,7 @@ class AlienInvation:
 
             #redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
--- ship.py
+++ ship.py
@@ -6,11 +6,11 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
 
-        self.screen = ai.screen
+        self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
