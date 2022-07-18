f1449ea02ec92ce6858ebfe8ecfd5adb4bed4f55
fadiabji <fadi.abji@htomail.com>
2022-07-18 12:58:14 +0200

    make continuese right and left movement to the ship

--- alien_invation.py
+++ alien_invation.py
@@ -23,6 +23,7 @@ class AlienInvation:
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update()
 
     def _check_events(self):
         """This method called helper method, it prfixs with _"""
@@ -30,6 +31,18 @@ class AlienInvation:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """This method called helper method, it prfixs with _"""
